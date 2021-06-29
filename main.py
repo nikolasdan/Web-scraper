14c34c87244589f2d36f2f39279487dab8d267e8
nikolasdan <nikolasdan16@protonmail.com>
2021-06-29 15:29:54 +0300

    scraper

---
+++ main.py
@@ -0,0 +1,19 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL = "https://www.cel.ro/calculatoare-desktop/"
+page = requests.get(URL)
+soup = BeautifulSoup(page.content, 'html.parser')
+content = soup.find(class_="content-wrapper")
+products = content.find_all(class_="product_data")
+
+for data in products:
+    title_prod = data.find(class_="productTitle")
+    link_prod = data.find('a')['href']
+    price_prod = data.find(class_="price")
+    stoc_status = data.find(class_="infoStocElem")
+    print(title_prod.text)
+    print(link_prod)
+    print(price_prod.text + " lei")
+    print(stoc_status.text)
+    print("\n")
